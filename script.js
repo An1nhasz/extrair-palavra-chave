90d1755074452d9ea60b26bf8f1b572d9346bccd
An1nhasz <ana.rodrigues.souza29@escola.pr.gov.br>
2025-09-26 17:06:54 -0300

    2609

--- script.js
+++ script.js
@@ -13,18 +13,47 @@ function mostraPalavraschave () {
 }
 
 function processaTexto(texto) {
-   let palavras = texto.split(/\P{L}+/u);
+     let palavras = texto.split(/\P{L}+/u);
 
 for (let i in palavras) {
-    palavras []
+    palavras [i] = palavras[i].toLowerCase();
 }
 
-   palavras
+   palavras = tirapalavrasruins(palavras);
+
+      const frequencias = contafrequencias(palavras);
+     let ordenadas = Object.keys(frequencias).sort(ordenapalavra);              {
+
+        function ordenapalavra(p1, p2){
+            return frequencias[p2]-frequencias[p1]
+        }
+
+    return ordenadas.slice(0, 10);
+    }
+
+  function contafrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras){
+        frequencias [i] = 0;
+        for (let j of palavras){
+             if (i == j){
+               frequencias[i]++;
+             }
+        }
+    }
+ return frequencias;
+}
+}
+   function tirapalavrasruins(palavras) {
+    const palavrasboas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2)  {
+            palavrasboas.push(palavra);
+        }  
+        }
+     return palavrasboas;
+    }
 
 
-                  {
-
- }
-
- 
- }
+    
+    
